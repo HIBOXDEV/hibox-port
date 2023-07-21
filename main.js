c1f2e27f60edaf7abc45b4aea0c4586d3be843e6
Hamza Ait <hait74144@gmail.com>
2023-07-21 20:17:34 +0200

    Act

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".autotype",{
-    strings: ["Web Developer" , "I am 17 years old" ] ,
+    strings: ["Web Developer" , "I am 15 years old" ] ,
     typeSpeed: 50,
     backSpeed: 50,
     loop: true
