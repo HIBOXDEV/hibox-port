0144f46c851f563bf6c6a5c205d17deb07df9a7c
HamzaAit <hait74144@gmail.com>
2024-01-06 16:21:55 +0100

    Update age on website. (generated by blackbox.ai )

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".autotype",{
-    strings: ["Web Developer" , "I am 15 years old" ] ,
+    strings: ["Web Developer" , "I am 16 years old" ] ,
     typeSpeed: 50,
     backSpeed: 50,
     loop: true
