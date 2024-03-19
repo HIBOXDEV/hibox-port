bf2d96e8b096da85cffc4549db062a8a50970e91
HamzaAit <hait74144@gmail.com>
2024-03-19 14:39:32 +0100

    barriredone

--- main.js
+++ main.js
@@ -15,6 +15,47 @@ document.addEventListener("DOMContentLoaded", function() {
         if (!clicked) {
             audio.play();
             clicked = true;
+
+            // Ocultar el desplazamiento de la página durante la animación
+            document.body.style.overflow = "hidden";
+
+            // Espera 2 segundos antes de ejecutar la segunda función
+            setTimeout(function() {
+                animateYellowTape();
+            }, 2000);
         }
     });
+
+    function animateYellowTape() {
+        var yellowTape = document.querySelector(".yellow-tape");
+        var audio = document.getElementById("audioo");
+
+        var duration = 10000; // Duración de la animación en milisegundos (10 segundos)
+        var startTime = performance.now(); // Tiempo de inicio de la animación
+
+        function animate() {
+            var elapsedTime = performance.now() - startTime; // Tiempo transcurrido desde el inicio
+            var progress = elapsedTime / duration; // Progreso de la animación (entre 0 y 1)
+
+            // Calcula la posición horizontal de la cinta (de 0% a 100% de la ventana visible)
+            var newPosition = progress * 100;
+            yellowTape.style.transform = "translateX(" + newPosition + "vw)";
+
+            if (progress < 1) {
+                // Si la animación aún no ha terminado, programa el siguiente cuadro de animación
+                requestAnimationFrame(animate);
+            } else {
+                // Si la animación ha terminado, elimina el elemento del "yellow-tape" del HTML
+                yellowTape.remove();
+                // Restaura el desplazamiento de la página
+                document.body.style.overflow = "auto";
+            }
+        }
+
+        // Reproduce el sonido
+        audio.play();
+
+        // Inicia la animación
+        animate();
+    }
 });
