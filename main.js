f80e677d885816527b6efa431a136cf3c57ccd98
HamzaAit <hait74144@gmail.com>
2024-03-25 13:29:41 +0100

    jhj

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+var typed = new Typed(".autotype",{
+    strings: ["Web Developer" , "I am 16 years old" ] ,
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    
+});
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
     var redButton = document.querySelector(".red-button");
     var audio = document.getElementById("audio");
